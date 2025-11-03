49ded4a0970378d3a14594bed9dd540166465241
mithra-vp <mithravp004@gmail.com>
2025-11-03 13:02:45 +0530

    first commit

---
+++ src/embedding-expression/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Welcome = () => {
+    let name = "MITHRA";
+    return (
+        <div>
+            <h3>Hi {name}</h3>
+        </div>
+    )
+}
+
+export default Welcome
